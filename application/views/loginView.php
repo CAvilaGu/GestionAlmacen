5fcc0240761d68c7b3177ef9b3cd4f33dc6d088c
lnrd256 <lnrd256@gmail.com>
2015-09-27 17:33:27 -0430

    login subido
    NO ME JODAS ESA VAINA

--- application/views/loginView.php
+++ application/views/loginView.php
@@ -8,33 +8,23 @@
 	</head>
 	<body>
 		<div class="container">
-			<?php
-			$username = array('name' => 'username', 'placeholder' => 'Nombre de Usuario', 'class' => 'form-control', 'type' => 'text', 'autofocus');
-			$password = array('name' => 'password','placeholder' => 'Contraseña', 'class' => 'form-control', 'type' => 'password');
-			$submit = array('name' => 'submit', 'value' => 'Iniciar Sesión', 'title' => 'Iniciar sesión', 'class' => 'btn btn-lg btn-primary btn-block');
-			?>	
 			<div class="row">
 				<div class="col-md-6 col-md-offset-3">
 					<img id="logoInicio" class="img-responsive" src="assets/images/logo.png">
 					<h1 id="tituloInicio"> Sistema de Gestión de Almacén </h1>
 				</div>
 				<div class="col-md-4 col-md-offset-4">
-					<div class="panel-body">
-						<?=form_open(base_url().'loginControl/new_user')?>
-						<?=form_input($username)?> <p> <?=form_error('username')?> </p>
-						<?=form_password($password)?> <p> <?=form_error('password')?> </p>
-						<?=form_hidden('token',$token)?>
-						<?=form_submit($submit)?>
-						<?=form_close()?>
-						<?php 
-							if($this->session->flashdata('usuario_incorrecto'))
-								{
-						?>
-									<p> <?=$this->session->flashdata('usuario_incorrecto')?> </p>
-						<?php
-								}
-						?> 
-					</div>
+					<form action="" method="POST">
+						<div class="form-group">
+							<label for="username">Username</label>
+							<input type="text" name="username" id="username" class="form-control" placeholder="Username">
+						</div>
+						<div class="form-group">
+							<label for="password">Password</label>
+							<input type="password" name="password" id="password" class="form-control" placeholder="password">
+						</div>
+						<input type="submit" value="Iniciar sesion" class="btn btn-success">
+					</form>
 				</div>
 			</div>
 		</div>
