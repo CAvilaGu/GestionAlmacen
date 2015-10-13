89a207ba68e2661b24f9fd17ad213719e8ea63e0
Carlos Enrique <CAvilaGu@Gmail.com>
2015-10-13 09:37:31 -0430

    MENU: arreglos esteticos del menu y de menu1 definitivo

--- application/views/template/1menu.php
+++ application/views/template/1menu.php
@@ -31,19 +31,19 @@
                         <a   href="login.html"><i class="fa fa-pencil-square-o fa-3x text-justify"></i> Inventario</a>
                     </li> 
                     <li  >
-                        <a   href="#"><i class="fa fa-laptop fa-3x text-justify"></i>Almacenes<span class="fa arrow"></span></a>
+                        <a   href="#"><i class="fa fa-object-ungroup fa-3x text-justify"></i>Almacenes<span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
                             <li>
-                                <a  href="ListGalponesController"><i class="fa fa-qrcode text-justify"></i> Galpones</a>
+                                <a  href="ListGalponesController"><i class="fa fa-home text-justify"></i> Galpones</a>
                             </li>
                             <li>
-                                <a   href=""><i class="fa fa-bar-chart-o fa text-justify"></i>Movimientos<span class="fa arrow"></span></a>
+                                <a href="#"><i class="fa fa-arrows text-justify"></i>Movimientos<span class="fa arrow"></span></a>
                                     <ul class="nav nav-third-level">
                                     <li>
-                                        <a href="#">Internos</a>
+                                        <a href="#"><i class="fa fa-circle"></i>Internos</a>
                                     </li>
                                     <li>
-                                        <a href="#">Externos</a>
+                                        <a href="#"><i class="fa fa-circle-thin"></i>Externos</a>
                                     </li>
                                 </ul>
                             </li>  
