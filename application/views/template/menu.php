3ec9a520328443fccf5483bdd1f3e25143ceb4fe
Carlos Enrique <CAvilaGu@Gmail.com>
2015-10-13 09:15:14 -0430

    MENU: arreglos esteticos del menu y de menu1

--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -13,10 +13,10 @@
                         <a href="#"><i class="fa fa-exchange fa-3x text-justify"></i>Despacho<span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
                             <li>
-                                <a  href="tab-panel.html"><i class="fa fa-qrcode fa-3x text-justify"></i> Despacho Cliente</a>
+                                <a  href="tab-panel.html"><i class="fa fa-user text-justify"></i> Despacho Cliente</a>
                             </li>
                             <li>
-                                <a   href="chart.html"><i class="fa fa-bar-chart-o fa-3x text-justify"></i> Despacho Producción</a>
+                                <a   href="chart.html"><i class="fa fa-industry text-justify"></i> Despacho Producción</a>
                             </li>  
                         </ul>
                     </li>  
@@ -31,13 +31,13 @@
                         <a   href="login.html"><i class="fa fa-pencil-square-o fa-3x text-justify"></i> Inventario</a>
                     </li> 
                     <li  >
-                        <a   href=""><i class="fa fa-laptop fa-3x text-justify"></i> Almacenes</a>
+                        <a   href="#"><i class="fa fa-laptop fa-3x text-justify"></i>Almacenes<span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
                             <li>
-                                <a  href="ListGalponesController"><i class="fa fa-qrcode fa-3x text-justify"></i> Galpones</a>
+                                <a  href="ListGalponesController"><i class="fa fa-qrcode text-justify"></i> Galpones</a>
                             </li>
                             <li>
-                                <a   href=""><i class="fa fa-bar-chart-o fa-3x text-justify"></i> Movimientos</a>
+                                <a   href=""><i class="fa fa-bar-chart-o fa text-justify"></i>Movimientos<span class="fa arrow"></span></a>
                                     <ul class="nav nav-third-level">
                                     <li>
                                         <a href="#">Internos</a>
@@ -50,6 +50,5 @@
                         </ul>
                     </li> 
                 </ul>
-
             </div>
         </nav>  
